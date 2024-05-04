64f4900acbc7bf3fca34fa3e76e90040b5c61ea2
goodmirow-1 <57578975+goodmirow-1@users.noreply.github.com>
2024-05-04 23:57:59 +0900

    fix: redis tls

--- src/infrastructure/db/redis/redis-service.ts
+++ src/infrastructure/db/redis/redis-service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import Redis from 'ioredis'
-import type { ConnectionOptions } from 'tls'
 
 @Injectable()
 export class RedisService {
@@ -11,17 +10,14 @@ export class RedisService {
     constructor(private configService: ConfigService) {
         const host = process.env.REDIS_HOST
         const port = parseInt(process.env.REDIS_PORT, 10)
-        const redisTls: ConnectionOptions | undefined = process.env.REDIS_TLS ? { servername: host } : undefined
 
         this.redisClient = new Redis({
             host: host,
             port: port,
-            tls: redisTls,
         })
         this.subscriberClient = new Redis({
             host: host,
             port: port,
-            tls: redisTls,
         })
 
         this.clearTokensAndQueue()
